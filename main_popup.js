75f644193f34b316a8d5f03df56a5b8cf103cdab
dweggyness <dweggyness@github.com>
2020-11-07 18:57:39 +0800

    moved display time left functionality into its own function instead of main_popup.js

--- main_popup.js
+++ main_popup.js
@@ -1,45 +1,18 @@
 
-const remainingWorkTime = document.getElementById('remainingWorkTime')
+import displayRemainingWorkTime from './js/displayRemainingWorkTime.js'
 
-let timer;
-
-function displayWorkTimeLeft(timeLeft) {
-    hours = parseInt(timeLeft / 60, 10);
-    minutes = parseInt(timeLeft % 60, 10);
-
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-
-    remainingWorkTime.textContent = hours + ":" + minutes;
-}
-
-function timerToDecrementTimeLeft(timeLeft, delay) {
-    if (timeLeft > 0) { 
-        timer = setTimeout(() => {
-            displayWorkTimeLeft(timeLeft)
-            timeLeft -= 1;
-
-            timerToDecrementTimeLeft(timeLeft, 1000)
-        }, delay)
-    }
-}
-
-function setRemainingWorkTime(duration) {
-    let timeLeft = duration;
-    let tickDuration = (duration % 1) * 1000 // grab the seconds part of the duration
-    displayWorkTimeLeft(timeLeft)
-
-    clearTimeout(timer)
-    timerToDecrementTimeLeft(timeLeft, tickDuration)
-}
-
+// whenever popup is opened, request time left of current WorkShift ( if any, otherwise returns False )
 chrome.runtime.sendMessage({ message: 'GET_REMAINING_TIME' }, 
     remainingTime => {
-        if (remainingTime) setRemainingWorkTime(remainingTime)
+        if (remainingTime) displayRemainingWorkTime(remainingTime)
     }
 )
 
+
+
+
+// when user clicks 'Start Workshift'
 document.getElementById('start_workshift').addEventListener('click', function() {
     chrome.runtime.sendMessage({ message: 'START_TIMER', timerDuration: 90 })
-    setRemainingWorkTime(90)
+    displayRemainingWorkTime(90)
 })
