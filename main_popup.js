954fd97de9731f3e3cbe57b74425d8d24dc9cf17
dweggyness <dweggyness@github.com>
2020-11-07 19:01:33 +0800

    added functionality to set work shift duration

--- main_popup.js
+++ main_popup.js
@@ -9,10 +9,20 @@ chrome.runtime.sendMessage({ message: 'GET_REMAINING_TIME' },
 )
 
 
-
-
 // when user clicks 'Start Workshift'
 document.getElementById('start_workshift').addEventListener('click', function() {
-    chrome.runtime.sendMessage({ message: 'START_TIMER', timerDuration: 90 })
-    displayRemainingWorkTime(90)
+    const workShiftHours = document.getElementById('total_hours').value
+    const workShiftMinutes =  document.getElementById('total_minutes').value
+
+    let workShiftDurationInMinutes = 0;
+    if (workShiftHours) workShiftDurationInMinutes += 60 * workShiftHours
+    if (workShiftMinutes) workShiftDurationInMinutes += workShiftMinutes
+
+    if (workShiftDurationInMinutes > 0) {
+        chrome.runtime.sendMessage({ message: 'START_TIMER', timerDuration: workShiftDurationInMinutes })
+        displayRemainingWorkTime(workShiftDurationInMinutes)
+    } else { // user left the fields blank, show error 
+        // todo
+        alert('error!!')
+    }
 })
